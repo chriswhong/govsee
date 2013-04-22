60373edfce0839bc91a3532c9c51124ce824e744
Christopher Whong <chris@whong.org>
2013-04-22 14:39:29 -0400

    Started on programmatic viewbox changes
    Go to page2.php?gov=NYC to test new viewbox easing functions

--- js/govsee2.js
+++ js/govsee2.js
@@ -38,7 +38,7 @@ var GovSee = {
 			return [d.y, d.x];
 		});
 
-		vis = d3.select("#chart").append("svg:svg").attr("width", bodywidth + 200).attr("height", bodyheight).append("g").attr("transform", "translate(0,0)").attr("id", "viewport")
+		vis = d3.select("#chart").append("svg:svg").attr("width", bodywidth + 200).attr("height", bodyheight).append("g").attr("class","svgcontainer").attr("transform", "translate(0,0)").attr("id", "viewport")
 
 		d3.json("/api/govlist/id/" + gov_id, function(json) {
 			//d3.json("flare.json", function(json) {
@@ -59,13 +59,18 @@ var GovSee = {
 			//GovSee.toggle(root.children[1]);
 			//GovSee.toggle(root.children[0]);
 
+			//draw a red circle at the origin and every 100 pixels making a grid
+			for(var j = 1;j<=5000;j=j+100){
+			vis.append("svg:circle").style("fill","red").attr("r",5).attr("cx",j).attr("cy",0);
+			vis.append("svg:circle").style("fill","red").attr("r",5).attr("cx",0).attr("cy",j);
+			}
 			GovSee.update(root);
 		});
 
 	},
 
 	update : function(source) {
-		var duration = d3.event && d3.event.altKey ? 5000 : 500;
+		var duration = d3.event && d3.event.altKey ? 5000 : 800;
 
 		// compute the new height
 		var levelWidth = [1];
@@ -83,29 +88,27 @@ var GovSee = {
 		};
 		childCount(0, root);
 		var newHeight = d3.max(levelWidth) * 50;
-		console.log(newHeight);
+
 		// 20 pixels per line
 		//tree = tree.size([newHeight, w]).separation(function(d){return 100;});
-       tree = tree.size([newHeight, w]).separation(function(d){
-       	return GovSee.shrink(100, d.level, 1);
-       	
-       	});
-	
+		tree = tree.size([newHeight, w]).separation(function(d) {
+			return GovSee.shrink(100, d.level, 1);
+
+		});
 
 		// Compute the new tree layout.
 		var nodes = tree.nodes(root).reverse();
 
 		// Normalize for fixed-depth.
 		nodes.forEach(function(d) {
-			newy=500;
-			for(var j=0;j<d.depth;j++){
-				
+			newy = 500;
+			for (var j = 0; j < d.depth; j++) {
+
 				newy = newy + GovSee.shrink(500, d.depth, .57);
 			}
-			console.log("newy is :" + newy);
-			d.y=newy;
 			
-			
+			d.y = newy;
+
 		});
 
 		// Update the nodes…
@@ -116,41 +119,46 @@ var GovSee = {
 		// Enter any new nodes at the parent's previous position.
 		var nodeEnter = node.enter().append("svg:g").attr("class", "node").attr("transform", function(d) {
 			return "translate(" + source.y0 + "," + source.x0 + ")";
-		}).on("click", function(d) {
+		}).on("click", GovSee.click).on("dblclick", function(d) {
+			
+		
 			GovSee.toggle(d);
 			GovSee.update(d);
+			var x = d3.select(this).select("circle");
+		GovSee.moveViewBox(x);
 		});
 
-		nodeEnter.append("svg:circle").attr("r", 1e-6).style("fill", function(d) {
-			return d._children ? "lightsteelblue" : "#fff";
+		nodeEnter.append("svg:circle").attr("r", 1e-6).attr("r", function(d) {
+			return GovSee.shrink(25, d.level, GovSee.commonscale);
+		}).style("fill", function(d) {
+		
+			return d.children || d._children ? "#29aae3" : "ea307b";
+		}).style("stroke-width", 0).style("stroke", function(d) {
+
+			return d.children || d._children ? "#29aae3" : "ea307b";
 		});
 
 		nodeEnter.append("svg:text").attr("x", function(d) {
 			//return d.children || d._children ? -30 : -30;
-			return GovSee.shrink(-30, d.level, GovSee.commonscale);
+			return GovSee.shrink(-36, d.level, GovSee.commonscale);
 		}).attr("dy", ".35em").attr("text-anchor", function(d) {
 			return d.children || d._children ? "end" : "end";
 		}).text(function(d) {
 			return d.name;
 		}).style("font", function(d) {
-      return GovSee.shrink(25, d.level, GovSee.commonscale) + "px ProximaNova";
-    });
+			return GovSee.shrink(25, d.level, GovSee.commonscale) + "px ProximaNova";
+		});
 
 		// Transition nodes to their new position.
 		var nodeUpdate = node.transition().duration(duration).attr("transform", function(d) {
 			return "translate(" + d.y + "," + d.x + ")";
 		});
 
-		nodeUpdate.select("circle").attr("r", function(d){
-			return GovSee.shrink(25, d.level, GovSee.commonscale);
-			//console.log(GovSee.shrink(15, d.level, GovSee.commonscale));
-			//console.log(d.level);
-			return d.level;
-			
-			
-		}).style("fill", function(d) {
-			return d._children ? "lightsteelblue" : "#fff";
-		});
+		//nodeUpdate.select("circle").attr("r", function(d){
+		//	return GovSee.shrink(25, d.level, GovSee.commonscale);
+		//}).style("fill", function(d) {
+		//	return d._children ? "#29aae3" : "#fff";
+		//});
 
 		nodeUpdate.select("text").style("fill-opacity", 1);
 
@@ -201,17 +209,87 @@ var GovSee = {
 			d.y0 = d.y;
 		});
 	},
-	 //reduces, pass it the original value and the level
-  shrink: function(x, level, rate) {
+	//reduces, pass it the original value and the level
+	shrink : function(x, level, rate) {
 
-    for ( j = 0; j < level; j++) {
+		for ( j = 0; j < level; j++) {
 
-      x = (x/2) / rate;
-    }
+			x = (x / 2) / rate;
+		}
 
-    return x;
+		return x;
 
-  },
+	},
+
+	// Click
+	click : function(d) {
+
+		var prev = d3.selectAll("g.node circle");
+		prev.transition().style("fill", function(d) {
+
+			return d.children || d._children ? "#29aae3" : "ea307b";
+		}).style("stroke-width",0);
+		
+		
+		var x = d3.select(this).select("circle");
+
+		x.transition().style("fill", "#ffffff").style("stroke-width", GovSee.shrink(16, d.level, GovSee.commonscale)).style("stroke", function(d) {
+
+			return d.children || d._children ? "#29aae3" : "ea307b";
+		});
+		
+		GovSee.moveViewBox(x);
+		
+		var s = document.getElementById("boxName");
+		s.innerHTML = d.name;
+
+		var s = document.getElementById("personName");
+		s.innerHTML = d.personname;
+
+		var uid = d.uid || 0;
+		pullUrl = "/api/govdetail/" + gov_id + "/id/" + uid;
+
+		$.ajax({
+			type : "GET",
+			url : pullUrl,
+			dataType : "json",
+			success : function(data) {
+				var s = document.getElementById("boxText");
+				s.innerHTML = data.node;
+
+				var s = document.getElementById("personText");
+				s.innerHTML = data.person;
+
+			}
+		});
+
+		$('#uid').val(d.uid);
+		$('#index').val(d.index);
+		GovSee.update();
+
+	},
+	
+	moveViewBox : function(x) {
+			var g = d3.select("g.svgcontainer");
+		g.transition().duration(800).attr("transform", function(d){
+			
+			c = x.data();
+				
+		
+			
+			console.log("c.x: " + c[0].x);
+			console.log("c.y: " + c[0].y);
+		
+			//return "matrix(\"" + 0.9,0,0,0.9, + 500 + "," + 500 +"\")"
+			
+			s = "matrix(" + 1 + "," + 0 + "," + 0 + "," + 1 +","+ ((c[0].y-500)*-1) + "," + ((bodyheight/2)-c[0].x) + ")"; 
+			console.log(s);
+			
+			return s;
+
+});
+	},
+	
 }
 
 $(function() {
